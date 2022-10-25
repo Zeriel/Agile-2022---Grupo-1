28e262732de8214f8e065abfdaee52f48a8b9f19
David Ezequiel <valverdedavid683@gmail.com>
2022-10-25 01:00:07 -0300

    idea summary component

---
+++ client/src/components/IdeaSummary.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import styled from 'styled-components'
+import smart from '../images/smart.png'
+
+function IdeaSummary() {
+  return (
+    <>
+      <Idea>
+        <div className="head">
+            <p className="title">Mesita de Luz Smart</p>
+            <img className="like" src="https://img.icons8.com/emoji/48/000000/green-heart.png"/>
+        </div>
+        <div className="content">
+            <img src={smart} alt="Smart"/>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+                typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+                with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+      </Idea>
+    </>
+  )
+}
+
+export default IdeaSummary
+
+const Idea = styled.div`
+    background-color: white;
+    height: 450px;
+    border-radius: 10px;
+    box-shadow: 0px 5px 15px 1px #aaa;
+    margin: 20px 10px;
+    padding: 1px 30px;
+    .head{
+        display: flex;
+        justify-content: space-between;
+        .title{
+            font-size: 3rem;
+            margin: 5px 0;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            @media (max-width: 768px) {
+                font-size: 1.5rem;
+            }
+        }
+        .like{
+            align-self: center;
+        }
+    }
+    .content{
+        display: flex;
+        margin-top: 25px;
+        img{
+            width: 300px;
+            /* mantiene la imagen centrada para que no se deforme */
+            margin: auto;
+            text-align: center;
+        }
+        p{
+            margin: 0 30px;
+            font-size: 1.2rem;
+            line-height: 1.5;
+            height: calc(300px * 0.6);
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 6;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+        @media (max-width: 768px) {
+            flex-direction: column;
+            p{
+                margin-top: 10px;
+                font-size: .9rem;
+                height: calc(200px * 0.55);
+                -webkit-line-clamp: 5;
+            }
+            img{
+                width: 200px;
+            }
+        }
+    }
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        height: auto;
+        padding-bottom: 20px;
+        position: relative;
+        z-index: -10;
+    }
+`
