10754d3d44a59a77d907ff75c5b3a4485701e302
Jaqui <jaquemonzon31@gmail.com>
2022-10-28 13:00:59 -0300

    Update IdeaFull.jsx

--- client/src/components/IdeaFull.jsx
+++ client/src/components/IdeaFull.jsx
@@ -38,19 +38,21 @@ function IdeaFull(props) {
 
         <p className="historia-title"><strong>Así Comienza Nuestra Historia</strong></p>
         
-        <p className="historia-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
-                    typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
-                    with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iure enim rem mollitia eveniet ea. Dolor quibusdam consequatur commodi, aliquam molestias pariatur, 
-                    officiis libero ratione ea, a ipsum quis aperiam.
+        <p className="historia-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+          with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iure enim rem mollitia eveniet ea. Dolor quibusdam consequatur commodi, aliquam molestias pariatur, 
+          officiis libero ratione ea, a ipsum quis aperiam.
         </p>
-        <p className="historia-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
-                    typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
-                    with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iure enim rem mollitia eveniet ea. Dolor quibusdam consequatur commodi, aliquam molestias pariatur, 
-                    officiis libero ratione ea, a ipsum quis aperiam.
+        <p className="historia-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+          with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iure enim rem mollitia eveniet ea. Dolor quibusdam consequatur commodi, aliquam molestias pariatur, 
+          officiis libero ratione ea, a ipsum quis aperiam.
         </p>
       </div>
     </>
