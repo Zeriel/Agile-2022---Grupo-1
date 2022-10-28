c1a576e19ad62e3514ecdc0da6e547e838a2cc82
David Ezequiel <valverdedavid683@gmail.com>
2022-10-27 22:30:09 -0300

    basic routing added

---
+++ client/src/components/IdeaFull.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function IdeaFull() {
+  return (
+    <div>IdeaFull</div>
+  )
+}
+
+export default IdeaFull
--- client/src/components/IdeaSummary.jsx
+++ client/src/components/IdeaSummary.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import smart from '../images/smart.png'
 import emptyLike from '../images/empty-like.png'
 import filledLike from '../images/filled-like.png'
+import { Link } from 'react-router-dom'
 
 
 function IdeaSummary(props) {
@@ -18,13 +19,14 @@ function IdeaSummary(props) {
             <p className="title">Mesita de Luz Smart</p>
             <img className="like" src={liked ? filledLike : emptyLike} onClick={handleLike} alt="like button"/>
         </div>
-        <div className="content">
+        <div className="content" onClick={props.clicked}>
             <img src={smart} alt="Smart"/>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
                 typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
                 with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
+        <Link className="ver-mas" onClick={props.clicked}>Más informción...</Link>
       </Idea>
     </>
   )
@@ -39,6 +41,7 @@ const Idea = styled.div`
     box-shadow: 0px 5px 15px 1px #aaa;
     margin: 20px 10px;
     padding: 1px 30px;
+    position: relative;
     .head{
         display: flex;
         justify-content: space-between;
@@ -94,6 +97,17 @@ const Idea = styled.div`
                 width: 200px;
             }
         }
+    }
+    .ver-mas{
+        font-size: 1.2rem;
+        position: absolute;
+        bottom: 20px;
+        right: 25px;
+        text-decoration: none;
+        color: #306d2e;
+    }
+    .ver-mas:hover{
+        color: black;
     }
     @media (max-width: 768px) {
         display: flex;
