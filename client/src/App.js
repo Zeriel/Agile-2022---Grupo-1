4e6f1b8dbf968d55a2a1882df5d6903d52a90788
SabbathRubio <92902391+SabbathRubio@users.noreply.github.com>
2022-10-28 00:02:59 -0300

    formulario carga idea
    Formulario carga idea de emprendedor

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import IdeaSummary from './components/IdeaSummary';
+import FormIdea from './components/FormIdea';
 
 function App() {
   return (
     <>
       <Navbar/>
       <IdeaSummary/>
+      
     </>
   );
 }
